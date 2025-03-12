18d571c04e7113a0d4329e65faab5f98174f3861
happymaker <happymaker1024@gmail.com>
2025-03-12 16:59:48 +0900

    요청이미지를 json으로 직접받아서 처리하는 방법

--- 2-1.dalle_app.py
+++ 2-1.dalle_app.py
@@ -2,7 +2,11 @@ import streamlit as st
 # openai key 값 로딩, 환경변수 .env에 저장, git에 업로딩은 안되도록
 from openai import OpenAI
 from dotenv import load_dotenv
+import base64
+import io
+from PIL import Image
 import os
+
 
 # 환경변수 로딩
 load_dotenv(override=True)
@@ -15,20 +19,21 @@ api_key = os.environ.get("OPENAI_API_KEY")
 # OpenAI 객체 생성
 client = OpenAI(api_key=api_key)
 
-# openai에 이미지 생성 요청 함수 정의
-# return 값이 생성 url
+# 실제이미지 값을 json값 리턴받음
 def get_image(user_prompt):
     response = client.images.generate(
             model="dall-e-3",
             prompt=user_prompt,
             size = "1024x1024",
             quality="standard",            
+            response_format='b64_json', # 이때 Base64 형태의 이미지를 리턴함
             n=1,
             
         )
-    image_url=response.data[0].url
-    # print(image_url)
-    return image_url
+    response = response.data[0].b64_json # DALLE로부터 Base64 형태의 이미지를 얻음.
+    image_data = base64.b64decode(response) # Base64로 쓰여진 데이터를 이미지 형태로 변환
+    image = Image.open(io.BytesIO(image_data)) # '파일처럼' 만들어진 이미지 데이터를 컴퓨터에서 볼 수 있도록 Open
+    return image
 
 
 # 1. 사용자에게 보여지는 부분 구현
@@ -49,11 +54,9 @@ if st.button("Painting"):
     print("user_prompt: ", user_prompt)
     if user_prompt:
         # print("프롬프트 값 정상")
-        # -------
-        # openAI에 그림그리기 메시지 보내기
-        image_url = get_image(user_prompt)
-        # --------
-        st.image(image_url, width=300, caption="로봇 화가 그림")
+        # openAI에 그림그리기 메시지 보내기, 함수 호출
+        image = get_image(user_prompt)
+        st.image(image, width=300, caption="로봇 화가 그림")
     else:
         # 그림기리기 요청을 입력하세요.
         st.write("텍스트 박스에 그림을 그릴 설명을 영어로 입력하세요.")
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ai_streamlit_ex
 openai 와 streamlit을 활용한 서비스 개발
 
-# 설치 라이브러리
+## 기본 설치 라이브러리
 ```
 pip install openai
 pip install streamlit
 ```
+
+## openai에서 이미지를 직접 리턴받을 때 필요
+```
+pip install Pillow
+```
+## 실행 초기 화면
+<img src="images/default_view.png" alt="초기화면">
